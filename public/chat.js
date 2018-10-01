fbcc680ac5bdbc377efb83c575a38c987d48650d
Chris Messina <clewismessina@gmail.com>
2018-09-30 21:48:22 -0500

    update chat.js to remove http vulnerability

--- public/chat.js
+++ public/chat.js
@@ -1,5 +1,5 @@
 // Make connection
-var socket = io.connect('http://localhost:3000');
+var socket = io.connect(process.env.PORT || 3000);
 
 // Query DOM
 var message = document.querySelector('#message'),
